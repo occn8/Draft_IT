a72612085025e48e291290ca3a9032d11e25c7d4
occn8 <ochenwayward@gmail.com>
2020-08-29 13:01:25 +0300

    fonts&index

--- lib/index.dart
+++ lib/index.dart
@@ -1,8 +1,22 @@
-
+//dependencies
 export 'dart:io';
 export 'package:path_provider/path_provider.dart';
 export 'package:sqflite/sqflite.dart';
 export 'package:flutter/foundation.dart';
 export 'package:shared_preferences/shared_preferences.dart';
 
+//screens
+export 'package:Draft_IT/screens/home.dart';
+export 'package:Draft_IT/screens/add.dart';
+export 'package:Draft_IT/screens/detail.dart';
+//models
 export 'package:Draft_IT/models/draft_model.dart';
+//utils
+export 'package:Draft_IT/utils/dbhelper.dart';
+//widgets
+
+//theme
+export 'package:Draft_IT/themes/light.dart';
+export 'package:Draft_IT/themes/dark.dart';
+//providers
+export 'package:Draft_IT/providers/themeprovider.dart';

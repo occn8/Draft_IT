afa5cd60ba6382db014e99c5af6c7faea9917733
occn8 <ochenwayward@gmail.com>
2020-08-30 08:56:33 +0300

    ontrack

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -23,7 +23,10 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: getDraftsList(),
+        body: ListView(children: [
+          Container(color: Colors.amber, height: 40,),
+          DraftBuilder(drafts: noteList)
+        ],),
         // body: Column(
         //   children: [
         //     Container(
@@ -66,8 +69,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   getDraftsList() {
-    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
-
     return ListView.builder(
         itemCount: count,
         itemBuilder: (BuildContext context, int position) {
@@ -82,7 +83,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               title: Text(
                 this.noteList[position].title,
-                style: titleStyle,
               ),
               subtitle: Text(this.noteList[position].date),
               trailing: GestureDetector(
@@ -156,5 +156,84 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       });
     });
+  }
+}
+
+class DraftBuilder extends StatefulWidget {
+  final List<Draft> drafts;
+  DraftBuilder({@required this.drafts});
+  @override
+  _DraftBuilderState createState() => _DraftBuilderState();
+}
+
+class _DraftBuilderState extends State<DraftBuilder> {
+
+  List<Widget> draftList() {
+    List<Widget> draftItemList = List();
+
+    for (int i = 0; i < widget.drafts.length; i++) {
+
+      draftItemList.add(draftItem(
+        draft: widget.drafts[i],
+        widget: getPriorityIcon(this.widget.drafts[i].priority),
+        color: getPriorityColor(this.widget.drafts[i].priority),
+        ontap1: () {},
+        ontap2: (){},
+      ));
+    }
+    return draftItemList;
+  }
+
+  Widget draftItem({Draft draft, Widget widget, Color color, Function ontap1, Function ontap2}) {
+    return Card(
+      color: Colors.white,
+      elevation: 2,
+      child: ListTile(
+        leading: CircleAvatar(
+          child: widget,
+          backgroundColor: color,
+        ),
+        title: Text(
+          draft.title,
+        ),
+        subtitle: Text(draft.description),
+        trailing: GestureDetector(
+            onTap: ontap1, child: Icon(Icons.delete, color: Colors.grey)),
+        onTap: ontap2,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: draftList(),
+    );
+  }
+
+  Color getPriorityColor(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.yellow;
+        break;
+      default:
+        return Colors.yellow;
+    }
+  }
+
+  Icon getPriorityIcon(int priority) {
+    switch (priority) {
+      case 1:
+        return Icon(Icons.play_arrow);
+        break;
+      case 2:
+        return Icon(Icons.keyboard_arrow_right);
+        break;
+      default:
+        return Icon(Icons.keyboard_arrow_right);
+    }
   }
 }
